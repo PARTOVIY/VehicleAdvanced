6e018fd6416dbdaac2c4aff59baa87c423c46419
PARTOVIY <49589187+PARTOVIY@users.noreply.github.com>
2022-06-06 20:11:03 +0500

    Add files via upload

--- VehicleAdvanced/Module.cs
+++ VehicleAdvanced/Module.cs
@@ -15,23 +15,47 @@ namespace VehicleAdvanced
     public class Module
     {
 
-        [ScriptFunction("get_healthMax")]
+        [ScriptFunction("get_maxHealth")]
         public static ushort getHealthMax([ScriptInstance] ExpressionValue instance)
         {
             if (!(instance.Data is VehicleClass vehicleClass)) return 0;
-            return vehicleClass.Vehicle.asset.healthMax;
+            return vehicleClass.Vehicle.asset.health;
         }
-        [ScriptFunction("set_speed")]
-        public static void setSpeed([ScriptInstance] ExpressionValue instance, ushort value)
+        [ScriptFunction("get_maxSpeed")]
+        public static float getMaxSpeed([ScriptInstance] ExpressionValue instance)
         {
-            if (!(instance.Data is VehicleClass vehicleClass)) return;
-            vehicleClass.Vehicle.asset.GetType().GetField("_speedMax", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(vehicleClass.Vehicle.asset, value);
+            if (!(instance.Data is VehicleClass vehicleClass)) return 0;
+            return vehicleClass.Vehicle.asset.speedMax;
         }
         [ScriptFunction("get_speed")]
         public static float getSpeed([ScriptInstance] ExpressionValue instance)
         {
             if (!(instance.Data is VehicleClass vehicleClass)) return 0;
             return vehicleClass.Vehicle.speed;
+        }
+        [ScriptFunction("get_battery")]
+        public static ushort getBattery([ScriptInstance] ExpressionValue instance)
+        {
+            if (!(instance.Data is VehicleClass vehicleClass)) return 0;
+            return vehicleClass.Vehicle.batteryCharge;
+        }
+        [ScriptFunction("set_battery")]
+        public static void setBattery([ScriptInstance] ExpressionValue instance, ushort value)
+        {
+            if (!(instance.Data is VehicleClass vehicleClass)) return;
+            vehicleClass.Vehicle.askChargeBattery(value);
+        }
+        [ScriptFunction("carjack")]
+        public static void carjack([ScriptInstance] ExpressionValue instance, Vector3Class force, Vector3Class torque)
+        {
+            if (!(instance.Data is VehicleClass vehicleClass)) return;
+            VehicleManager.carjackVehicle(vehicleClass.Vehicle, null, force.Vector3, torque.Vector3);
+        }
+        [ScriptFunction("removeAllPassengers")]
+        public static void removeAllPassengers([ScriptInstance] ExpressionValue instance)
+        {
+            if (!(instance.Data is VehicleClass vehicleClass)) return;
+            vehicleClass.Vehicle.forceRemoveAllPlayers();
         }
     }
 }
